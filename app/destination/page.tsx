08690d0d86397154717a15bf94e403124d630160
Yasin Walum <ywalum@gmail.com>
2024-05-25 13:25:49 +0300

    added routes

--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import destinations from "@/components/data.json";
 
 const images = {
-    mobile: "/assets/home/background-home-mobile.jpg",
-    tablet: "/assets/home/background-home-tablet.jpg",
-    desktop: "/assets/home/background-home-desktop.jpg",
+    mobile: "/assets/destination/background-destination-mobile.jpg",
+    tablet: "/assets/destination/background-destination-tablet.jpg",
+    desktop: "/assets/destination/background-destination-desktop.jpg",
 };
 
 export default function Home() {
@@ -33,29 +34,16 @@ export default function Home() {
                         <h3
                             className={` font-normal text-sm font-barlowcondensed `}
                         >
-                            SO, YOU WANT TO TRAVEL TO
+                            destination
                         </h3>
                         <h1
                             className={`text-7xl my-[2rem]  text-white font-normal font-bellefair `}
                         >
                             SPACE
                         </h1>
-                        <p className=" max-w-[50ch] ">
-                            Let’s face it; if you want to go to space, you might
-                            as well genuinely go to outer space and not hover
-                            kind of on the edge of it. Well sit back, and relax
-                            because we’ll give you a truly out of this world
-                            experience!
-                        </p>
+                        <p className=" max-w-[50ch] ">text</p>
                     </div>
-                    <div className=" relative cursor-pointer group ">
-                        <div
-                            className={` grid place-items-center aspect-square bg-white w-fit text-black p-[3rem] rounded-full text-sm font-semibold tracking-wider  `}
-                        >
-                            EXPLORE
-                        </div>
-                        <div className=" opacity-0 group-hover:opacity-100 transition-all duration-300 absolute w-[150%] bg-white/15 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 aspect-square rounded-full " />
-                    </div>
+                    <div>part two</div>
                 </div>
             </div>
         </section>
